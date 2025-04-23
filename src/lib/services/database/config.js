91dc0a701ef347d886593cb3839cc61e7f52583c
S0mt0 <sewkito@gmail.com>
2025-04-23 14:39:45 +0100

    updated sequelize migrations config

--- src/lib/services/database/config.js
+++ src/lib/services/database/config.js
@@ -29,9 +29,12 @@ module.exports = {
       },
     },
     pool: {
-      max: 1,
-      min: 0,
+      max: 10,
+      min: 1,
+      acquire: 30000,
       idle: 10000,
     },
+
+    timezone: 'UTC',
   },
 };
