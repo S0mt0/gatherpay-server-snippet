21d881dfa905765ae6cda95e363e9eddcf3ec01d
S0mt0 <sewkito@gmail.com>
2025-04-23 14:38:24 +0100

    ran database migration

--- src/lib/services/database/config.js
+++ src/lib/services/database/config.js
@@ -1,7 +1,9 @@
 // Note To Devs: To use this migrations configuration, spin up the database service using docker with the same exact setup provided on the docker-compose.yml file. Hint: you only have to ensure that the docker engine is running, then use the scripts provided in the package.json file to run your containers.
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
+    url:
+      process.env.DATABASE_URL ||
+      'postgres://sewkito:123@127.0.0.1:5434/gatherpay',
     dialect: 'postgres',
     dialectOptions: {
       bigNumberStrings: true,
@@ -21,6 +23,15 @@ module.exports = {
     dialect: 'postgres',
     dialectOptions: {
       bigNumberStrings: true,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      max: 1,
+      min: 0,
+      idle: 10000,
     },
   },
 };
--- src/lib/services/database/migrations/20250423130021-update-session-model.js
+++ src/lib/services/database/migrations/20250423130021-update-session-model.js
@@ -2,19 +2,19 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Sessions', 'twoFactorSecret', {
+    await queryInterface.addColumn('sessions', 'twoFactorSecret', {
       type: Sequelize.STRING,
       allowNull: true,
     });
 
-    await queryInterface.addColumn('Sessions', 'twoFactorLoggedIn', {
+    await queryInterface.addColumn('sessions', 'twoFactorLoggedIn', {
       type: Sequelize.BOOLEAN,
       defaultValue: false,
     });
   },
 
   async down(queryInterface) {
-    await queryInterface.removeColumn('Sessions', 'twoFactorSecret');
-    await queryInterface.removeColumn('Sessions', 'twoFactorLoggedIn');
+    await queryInterface.removeColumn('sessions', 'twoFactorSecret');
+    await queryInterface.removeColumn('sessions', 'twoFactorLoggedIn');
   },
 };
